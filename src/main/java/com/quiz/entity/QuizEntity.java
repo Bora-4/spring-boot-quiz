666900cc3745d4014bd28c097ae094650f54197b
Bora-4 <bora.rrena@fti.edu.al>
2024-05-30 01:14:47 +0200

    krijimi i QuizEntity

---
+++ src/main/java/com/quiz/entity/QuizEntity.java
@@ -0,0 +1,50 @@
+package com.quiz.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "quizzes")
+public class QuizEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "quiz_id", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuestionEntity> questions;
+
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PostPersist
+    protected void onUpdate(){
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
