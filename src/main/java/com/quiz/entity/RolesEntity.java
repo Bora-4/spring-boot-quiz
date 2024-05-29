daca7dc1dae2289dbb427451dcd2084928063851
Bora-4 <bora.rrena@fti.edu.al>
2024-05-30 00:21:29 +0200

    krijimi i RolesEntity

--- src/main/java/com/quiz/entity/RolesEntity.java
+++ src/main/java/com/quiz/entity/RolesEntity.java
@@ -1,4 +1,20 @@
 package com.quiz.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor @NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+
 public class RolesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // eshte best practice per te perdorur tip te dhene Long ne vend te Integer per primary keys
+
+    @Column(nullable = false, unique = true)
+    private String name;
 }
