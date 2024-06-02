745e3e0a748b7edd3108c096a20e2b1889453b48
Bora-4 <bora.rrena@fti.edu.al>
2024-06-03 00:25:22 +0200

    UserService dhe UserServiceImpl

--- src/main/java/com/quiz/service/UserService.java
+++ src/main/java/com/quiz/service/UserService.java
@@ -3,8 +3,13 @@ package com.quiz.service;
 import com.quiz.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface UserService {
-    void save(UserDTO user);
+    void save(UserDTO userDTO);
     UserDTO findByUsername(String username);
+    void update(UserDTO userDTO);
+    List<UserDTO> findAll();
+    void delete(Long id);
 }
--- src/main/java/com/quiz/service/impl/UserServiceImpl.java
+++ src/main/java/com/quiz/service/impl/UserServiceImpl.java
@@ -1,17 +1,59 @@
 package com.quiz.service.impl;
 
+
+import com.quiz.dto.UserDTO;
 import com.quiz.entity.UserEntity;
+import com.quiz.mapper.UserMapper;
 import com.quiz.repository.UserRepository;
 import com.quiz.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserServiceImpl implements UserService {
-    @Autowired
-    private UserRepository userRepository;
+import java.util.List;
 
-    public void save(UserEntity user){
-        userRepository.save(user);
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void save(UserDTO userDTO) {
+        UserEntity user = UserMapper.toEntity(userDTO);
+        this.userRepository.save(user);
+
+    }
+
+    @Override
+    public UserDTO findByUsername(String username) {
+        UserEntity user = this.userRepository.findByUsername(username);
+        if(user != null){
+            return UserMapper.toDTO(user);
+        }
+        throw new RuntimeException("User with username \"" +username+"\" does not exist. ");
+    }
+
+    @Override
+    public void update(UserDTO userDTO) {
+        UserEntity user = this.userRepository.findByUsername(userDTO.getUsername());
+        if(user != null){
+            this.userRepository.update(user);
+        }
+        throw new RuntimeException("User with username \"" +userDTO.getUsername()+"\" does not exist. ");
+
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return userRepository.findAll().stream().map(UserMapper::toDTO).toList();
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.delete(id);
     }
 }
