b24e5ae015bdd20745109c383aba0d640bad22d3
Bora-4 <bora.rrena@fti.edu.al>
2024-06-02 23:56:59 +0200

    krijimi i user repository dhe impl

--- src/main/java/com/quiz/repository/UserRepository.java
+++ src/main/java/com/quiz/repository/UserRepository.java
@@ -1,19 +1,16 @@
 package com.quiz.repository;
 
 import com.quiz.entity.UserEntity;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-
-    @Query("SELECT u FROM UserEntity u WHERE u.username = :username")
-    public UserEntity getUserEntitiesByUsername(@Param("username") String username);
-
-//    This interface is a subtype of CrudRepository defined by Spring Data JPA so Spring will
-//    generate implementation class at runtime. We define the getUserByUsername() method
-//    annotated by a JPA query to be used by Spring Security for authentication.
+public interface UserRepository {
+    void save(UserEntity userEntity);
+    UserEntity findUserByUsername(String username);
+    void update(UserEntity userEntity);
+    List<UserEntity> findAll();
+    void delete(Long id);
 
 }
---
+++ src/main/java/com/quiz/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,38 @@
+package com.quiz.repository.impl;
+
+import com.quiz.entity.UserEntity;
+import com.quiz.repository.UserRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import java.util.List;
+
+public class UserRepositoryImpl implements UserRepository {
+    @PersistenceContext
+    EntityManager em;
+    @Override
+    public void save(UserEntity userEntity) {
+        em.persist(userEntity);
+    }
+
+    @Override
+    public UserEntity findUserByUsername(String username) {
+        return em.find(UserEntity.class, username);
+    }
+
+    @Override
+    public void update(UserEntity userEntity) {
+        em.merge(userEntity);
+    }
+
+    @Override
+    public List<UserEntity> findAll() {
+        return em.createQuery("SELECT u FROM UserEntity u WHERE u.username = :username", UserEntity.class)
+                .getResultList();
+    }
+
+    @Override
+    public void delete(Long id) {
+        em.remove(id);
+    }
+}
